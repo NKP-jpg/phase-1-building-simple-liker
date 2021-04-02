b8d97fc245045b643411b8960e061bc7b9acb3fe
Nija <nija.perry21@gmail.com>
2021-04-02 18:36:28 -0400

    commit tree fc070466d074ec4972dbfd4806f88f57e96475e9

--- main.js
+++ main.js
@@ -5,15 +5,27 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+/*Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads*/
+document.addEventListener("DOMContentLoaded", () => {
+  const pageLoad = document.getElementById(/*What goes in here? */  "modal").hidden = true;
+  pageLoad.addEventListener("submit", serverRequest)
+});
+
+/*When a user clicks on an empty heart:*/
+function serverRequest(event) {
+  event.preventDefault()
+    .addEventListener("click", /*what goes here?*/)
+
+}
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
